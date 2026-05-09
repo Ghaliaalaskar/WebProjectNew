54e81a4dfc128a81a49f35ca30e7ffca58df5072
Shaden Alosaimi <Shaden.m.alosaimi@gmail.com>
2026-05-09 21:13:09 +0300

    connected manage user delet to database

--- ManageUsers.aspx.cs
+++ ManageUsers.aspx.cs
@@ -1,17 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebProjectNew
 {
-    public partial class ManageUser : System.Web.UI.Page
+    public partial class ManageUsers : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-    }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["PartyPlannerDB"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "DELETE FROM Users WHERE Email=@Email";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtPhone.Text = "";
+
+            //User deleted successfully
+        }
+    }  
 }
