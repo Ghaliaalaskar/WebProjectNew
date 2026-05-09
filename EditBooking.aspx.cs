75c1832aa2a790a2a6dce840f86552a323ebe94a
Shaden Alosaimi <Shaden.m.alosaimi@gmail.com>
2026-05-09 20:54:45 +0300

    connected editbooking  update to database

--- EditBooking.aspx.cs
+++ EditBooking.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebProjectNew
 {
@@ -11,6 +13,24 @@ namespace WebProjectNew
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
+            string cs = ConfigurationManager.ConnectionStrings["PartyPlannerDB"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "UPDATE Appointments SET AppointmentDate=@AppointmentDate, AppointmentTime=@AppointmentTime WHERE UserID=@UserID";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@AppointmentDate", txtDate.Text);
+            cmd.Parameters.AddWithValue("@AppointmentTime", ddlTime.Text);
+            cmd.Parameters.AddWithValue("@UserID", txtClientName.Text);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
             lblMessage.Text = "Booking updated successfully";
         }
     }
