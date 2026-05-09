91cb7c38377c7a7afcfe2ec3f473f834df323889
Shaden Alosaimi <Shaden.m.alosaimi@gmail.com>
2026-05-09 20:08:18 +0300

    finished connecting addthemes insert code

--- AddThemes.aspx.cs
+++ AddThemes.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebProjectNew
 {
@@ -13,6 +15,26 @@ namespace WebProjectNew
         {
             if (Page.IsValid)
             {
+                string cs = ConfigurationManager.ConnectionStrings["PartyPlannerDB"].ConnectionString;
+
+                SqlConnection con = new SqlConnection(cs);
+
+                string query = "INSERT INTO Services(ServiceName, Description, Duration, Price, ServicePhoto) VALUES(@ServiceName,@Description,@Duration,@Price,@ServicePhoto)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@ServiceName", txtName.Text);
+                cmd.Parameters.AddWithValue("@Description", txtDescription.Text);
+                cmd.Parameters.AddWithValue("@Duration", txtDuration.Text);
+                cmd.Parameters.AddWithValue("@Price", txtPrice.Text);
+                cmd.Parameters.AddWithValue("@ServicePhoto", FileUpload1.FileName);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
                 lblMessage.Text = "Theme added successfully";
             }
         }
