0f8e4213261b30cbbdc82a6c687b7c0df8a0f03f
Shaden Alosaimi <Shaden.m.alosaimi@gmail.com>
2026-05-09 20:50:05 +0300

    connected managebooking delet to database

--- ManageBookings.aspx.cs
+++ ManageBookings.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebProjectNew
 {
@@ -11,6 +13,21 @@ namespace WebProjectNew
 
         protected void btnDelete_Click(object sender, EventArgs e)
         {
+            string cs = ConfigurationManager.ConnectionStrings["PartyPlannerDB"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "DELETE FROM Appointments WHERE AppointmentDate=@AppointmentDate AND AppointmentTime=@AppointmentTime";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@AppointmentDate", txtThemeDate.Text);
+            cmd.Parameters.AddWithValue("@AppointmentTime", ddlTime.SelectedValue);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
             txtClientName.Text = "";
             txtThemeDate.Text = "";
             ddlTime.SelectedIndex = 0;
