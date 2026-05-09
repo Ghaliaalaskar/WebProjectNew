b51616e4edae0cf6ec8097b70103a511f6fac9ec
Shaden Alosaimi <Shaden.m.alosaimi@gmail.com>
2026-05-09 20:39:39 +0300

    connected adduser page to database

--- AddUser.aspx.cs
+++ AddUser.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebProjectNew
 {
@@ -13,6 +15,24 @@ namespace WebProjectNew
         {
             if (Page.IsValid)
             {
+                string cs = ConfigurationManager.ConnectionStrings["PartyPlannerDB"].ConnectionString;
+
+                SqlConnection con = new SqlConnection(cs);
+
+                string query = "INSERT INTO Users(Name, Email, Phone) VALUES(@Name,@Email,@Phone)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
                 lblMessage.Text = "User saved successfully";
             }
         }
