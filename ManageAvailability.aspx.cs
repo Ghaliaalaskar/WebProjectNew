3f7fa411dc9205484377e22ef4368cf1b4e2f195
Shaden Alosaimi <Shaden.m.alosaimi@gmail.com>
2026-05-09 20:47:35 +0300

    connected manageava delet to database

--- ManageAvailability.aspx.cs
+++ ManageAvailability.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebProjectNew
 {
@@ -11,10 +13,22 @@ namespace WebProjectNew
 
         protected void btnDelete_Click(object sender, EventArgs e)
         {
-            txtThemeName.Text = "";
-            txtDate.Text = "";
-            txtTime.Text = "";
-            ddlStatus.SelectedIndex = 0;
+            string cs = ConfigurationManager.ConnectionStrings["PartyPlannerDB"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "DELETE FROM Availability WHERE AvailableDate=@AvailableDate AND AvailableTime=@AvailableTime";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@AvailableDate", txtDate.Text);
+            cmd.Parameters.AddWithValue("@AvailableTime", txtTime.Text);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
 
             lblMessage.Text = "Deleted Successfully";
         }
